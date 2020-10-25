6d952721066d60945880988d9c4dd07a473f7a76
Ziwei Liu <ziwei.liu.23@gmail.com>
2020-10-25 14:23:12 +1300

    Update styling

---
+++ components/Toggle.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import styles from "./Toggle.module.scss";
+
+export default function Toggle({ check, onClick }) {
+  const [checked, setChecked] = useState(check);
+
+  return (
+    <div className={styles.toggleWrapper}>
+      <input
+        type="checkbox"
+        className="dn"
+        id="dn"
+        checked={checked}
+        onClick={(value) => {
+          setChecked(!checked);
+          onClick(value.target);
+        }}
+      />
+      <label htmlFor="dn" className={styles.toggle}>
+        <span className={styles.toggleHandler}>
+          <span className={`${styles.crater} ${styles.crater1}`}></span>
+          <span className={`${styles.crater} ${styles.crater2}`}></span>
+          <span className={`${styles.crater} ${styles.crater3}`}></span>
+        </span>
+        <span className={`${styles.star} ${styles.star1}`}></span>
+        <span className={`${styles.star} ${styles.star2}`}></span>
+        <span className={`${styles.star} ${styles.star3}`}></span>
+        <span className={`${styles.star} ${styles.star4}`}></span>
+        <span className={`${styles.star} ${styles.star5}`}></span>
+        <span className={`${styles.star} ${styles.star6}`}></span>
+      </label>
+    </div>
+  );
+}
--- pages/day/[day].tsx
+++ pages/day/[day].tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import Layout from "../../components/layout";
 import { getAllDevoIds, getDevo } from "../api/data";
 import { GetStaticProps, GetStaticPaths } from "next";
+import Toggle from "@components/Toggle";
 
 declare const window: any;
 
@@ -26,24 +27,58 @@ export default function Devo({ daily }) {
   });
 
   return (
-    <div style={{ background: "lightpink" }}>
+    <div style={{ background: "#8a9ebf" }}>
       <svg
-        viewBox="0 0 40 10"
-        height={width / 4}
+        viewBox="0 0 40 16"
+        height={width / 2.5}
         width={width}
         style={{ background: "white" }}
       >
+        <defs>
+          <linearGradient id="grad1" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop
+              offset="0%"
+              style={{
+                stopColor: "#347993",
+                stopOpacity: 1,
+              }}
+            />
+            <stop
+              offset="100%"
+              style={{ stopColor: "#6aabcc", stopOpacity: 1 }}
+            />
+          </linearGradient>
+          <linearGradient id="grad2" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop
+              offset="0%"
+              style={{
+                stopColor: "#425691",
+                stopOpacity: 1,
+              }}
+            />
+            <stop
+              offset="100%"
+              style={{ stopColor: "#8a9ebf", stopOpacity: 1 }}
+            />
+          </linearGradient>
+        </defs>
         <path
-          d="M 0 0 C 2 1 3 5 4 6 C 6 8 7 6 10 8 C 13 10 16 8 19 7 C 23 6 23 8 28 6 C 33 4 35 4 40 6 L 40 10 L 0 10 L 0 0"
-          fill="lightpink"
+          d="M 0 0 C 2 1 3 5 4 6 C 6 8 7 6 10 8 C 13 11 14 11 16 11 C 20 11 22 9 27 9 C 33 9 31 5 40 4 L 40 15 L 0 15 L 0 0"
+          fill="url(#grad1)"
+        />
+        <path
+          d="M 0 6 C 3 8 3 10 6 11 C 10 12 12 9 17 9 C 21 9 23 11 26 11 C 34 11 33 7 40 7 L 40 16 L 0 16 L 0 6"
+          fill="url(#grad2)"
         />
       </svg>
       <Layout>
         <Head>
           <title>{`Day ${daily.day}`}</title>
         </Head>
-
-        {devo.time}
+        <Toggle
+          check={devo.time === "pm"}
+          onClick={(value) => setDevo(daily.devo[value.checked ? 1 : 0])}
+        />
         <p>{devo.keyVerse}</p>
         <p>{devo.verseRef}</p>
         <p>{devo.body}</p>
