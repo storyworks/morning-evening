bbbf711499028cf605c3c8c8e3ef78eb33fe4f46
Ziwei Liu <ziwei.liu.23@gmail.com>
2020-12-27 12:18:33 +1300

    Update data structure

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -2,18 +2,18 @@ import devotionals from "./data.json";
 
 export function getAllDevoIds() {
   return devotionals.map((devo, index) => {
-    const day = 1 + Math.floor(index / 2);
+    const day = 1 + index;
     return {
       params: {
-        day: day.toString(),
+        day: day.toString()
       },
     };
   });
 }
 
 export function getDevo(day: number) {
-  const index = (day - 1) * 2;
-  const devo = [devotionals[index], devotionals[index + 1]];
+  const index = (day - 1);
+  const devo = devotionals[index];
   return {
     day,
     devo,
--- pages/day/[day].tsx
+++ pages/day/[day].tsx
@@ -18,70 +18,25 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 };
 
 export default function Devo({ daily }) {
-  console.log(daily);
   const hour = new Date().getHours();
-  const [devo, setDevo] = useState(daily.devo[hour <= 14 ? 0 : 1]);
+  const [isEvening, setEvening] = useState(hour >= 15)
+  const [devo, setDevo] = useState(daily.devo.content[isEvening ? 1 : 0]);
   const [width, setWidth] = useState(0);
 
   useEffect(() => {
     setWidth(window.innerWidth);
   });
 
+  useEffect(() => {
+    setDevo(daily.devo.content[isEvening ? 1 : 0])
+  }, [isEvening])
+
   return (
     <div style={{ background: "#fff" }}>
       <Toggle
-        check={devo.time === "pm"}
-        onClick={(value) => setDevo(daily.devo[value.checked ? 1 : 0])}
+        check={isEvening}
+        onClick={(value) => setEvening(value.checked)}
       />
-      <svg
-        viewBox="0 0 40 16"
-        height={width / 2.5}
-        width={width}
-        style={{ background: "white" }}
-      >
-        <defs>
-          <linearGradient id="grad1" x1="0%" y1="0%" x2="0%" y2="100%">
-            <stop
-              offset="0%"
-              style={{
-                stopColor: devo.time === "am" ? "#fff" : "#347993",
-                stopOpacity: 1,
-              }}
-            />
-            <stop
-              offset="100%"
-              style={{
-                stopColor: devo.time === "am" ? "#6aabcc" : "#6aabcc",
-                stopOpacity: 1,
-              }}
-            />
-          </linearGradient>
-          <linearGradient id="grad2" x1="0%" y1="0%" x2="0%" y2="100%">
-            <stop
-              offset="0%"
-              style={{
-                stopColor: devo.time === "am" ? "#425691" : "#425691",
-                stopOpacity: 1,
-              }}
-            />
-            <stop
-              offset="100%"
-              style={{
-                stopColor: devo.time === "am" ? "#8a9ebf" : "#8a9ebf",
-                stopOpacity: 1,
-              }}
-            />
-          </linearGradient>
-        </defs>
-        <path
-          d="M 0 0 C 2 1 3 5 4 6 C 6 8 7 6 10 8 C 13 11 14 11 16 11 C 20 11 22 9 27 9 C 33 9 31 5 40 4 L 40 15 L 0 15 L 0 0"
-          fill="url(#grad1)"
-        />
-        <path
-          d="M 0 6 C 3 8 3 10 6 11 C 10 12 12 9 17 9 C 21 9 23 11 26 11 C 34 11 33 7 40 7 L 40 16 L 0 16 L 0 6"
-          fill="url(#grad2)"
-        />
-      </svg>
       <Layout>
         <Head>
           <title>{`Day ${daily.day}`}</title>
